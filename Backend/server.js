2677be4ecadc75458bea407cf5723814c51c38de
Pratik Devkota <114279879+Pratik58D@users.noreply.github.com>
2025-01-31 13:43:08 +0545

    cors updation

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const port = process.env.PORT || 7001;
 
 app.use(cors({
-    origin : "http://localhost:5174",
+    origin : "http://localhost:5173",
     methods : ['GET','POST','DELETE', "PUT"],
     allowedHeaders :[
         "Content-Type",
