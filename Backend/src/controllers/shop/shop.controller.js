a82fcb7a7bbb5a5dec74da6b4da6e46e4bb33e59
Pratik Devkota <114279879+Pratik58D@users.noreply.github.com>
2025-02-20 14:03:01 +0545

    search and sort Product

--- Backend/src/controllers/shop/shop.controller.js
+++ Backend/src/controllers/shop/shop.controller.js
@@ -1,15 +1,43 @@
-import Product from "../../models/product.model.js"
+import Product from "../../models/product.model.js";
 
 //fetching product for shop
-export const getFilterProducts = async(req,res) =>{
-    try {
-        const products = await Product.find({});
+export const getFilterProducts = async (req, res) => {
+  try {
+    const { category = [], brand = [], sortBy = "price-low-high" } = req.query;
+    let filters = {};
+    if (category.length) {
+      //we are dynamicaly adding category inside filter
+      filters.category = { $in: category.split(",") };
+    }
+    if (brand.length) {
+      //we are dynamicaly adding brand inside filter
+      filters.brand = { $in: brand.split(",") };
+    }
+    let sort = {};
+    switch (sortBy) {
+      case "price-low-high":
+        sort.price = 1;
+        break;
+      case "price-high-low":
+        sort.price = -1;
+        break;
+      case "a-z":
+        sort.title = 1;
+        break;
+      case "z-a":
+        sort.title = -1;
+        break;
 
-        res.status(200).json({ sucess: true, data: products });
+      default:
+        sort.title = 1;
+        break;
+    }
 
-        
-    } catch (error) {
-        console.log("error in shop product controller", error.message);
-        res.status(500).json({ sucess: false, message: "Server error" });
-      }
-}
+    const products = await Product.find(filters).sort(sort);
+
+    res.status(200).json({ sucess: true, data: products });
+  } catch (error) {
+    console.log("error in shop product controller", error.message);
+    res.status(500).json({ sucess: false, message: "Server error" });
+  }
+};
